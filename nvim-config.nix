97c9b3f52dc173c7ed0d90705bd35daf56ce76ca
Kevin Crook <kevinwaynecrook@gmail.com>
2026-02-02 21:36:39 -0600

    feat(nvim): Add basic nvim settings for Nix

--- nvim-config.nix
+++ nvim-config.nix
@@ -1,6 +1,39 @@
 {
   config.vim = {
-    theme.enable = true;
-    treesitter.enable = true;
+    theme = {
+      enable = true;
+      name = "onedark";
+      style = "darker";
+      transparent = true;
+    };
+
+    statusline = {
+      lualine = {
+        enable = true;
+        theme = "onedark";
+      };
+    };
+
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+      trouble.enable = true;
+      otter-nvim.enable = true;
+    };
+
+    languages = {
+      enableFormat = true;
+      enableTreesitter = true;
+
+      nix.enable = true;
+      markdown.enable = true;
+      bash.enable = true;
+    };
+
+    notes.todo-comments.enable = true;
+
+    options.mouse = "";
+
+    spellcheck.enable = true;
   };
 }
