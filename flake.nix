e57606c7a7cee69b89819802f6a84cb5faf7f544
Kevin Crook <kevinwaynecrook@gmail.com>
2026-02-02 20:08:10 -0600

    chore(flake): Flatten inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs-lib.follows = "nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-lib";
+
     nvf.url = "github:notashelf/nvf";
+    nvf.inputs.flake-parts.follows = "flake-parts";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
+    nvf.inputs.systems.follows = "systems";
+
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = {nixpkgs, ...} @ inputs: {
